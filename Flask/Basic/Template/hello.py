c43cf383bdae46dc2f48201b40914e6d8941c6e1
GeovannyMero <jefferson_mero1911@hotmail.com>
2021-09-17 00:22:10 -0500

    forms

--- Flask/Basic/Template/hello.py
+++ Flask/Basic/Template/hello.py
@@ -1,14 +1,30 @@
 from flask_bootstrap import Bootstrap
-from flask import Flask, render_template
+# from flask import Flask, render_template
+from flask.templating import render_template
+from flask_wtf import Form
+from flask import Flask
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 # bootstrap
 
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "Pruebas geo"
 bootstrap = Bootstrap(app)
+
+class NameForm(Form):
+    name = StringField("What is your name?", validators=[Required()])
+    submit = SubmitField("Submit")
+
 
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+    return render_template("index2.html", form=form, name=name)
 
 @app.route("/user/<name>")
 def user(name):
